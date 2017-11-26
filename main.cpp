ff17af2d2b732aec49737b331957bc78bac60e4f
Rob Farlow <robfarlow@gmail.com>
2017-11-26 16:24:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,16 +5,35 @@
 #include <fcntl.h>
 #include <ugpio/ugpio.h>
 #include <relay-exp.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+//GLOBAL VARIABLES-------------------------
 const int ticrate = 20;
 const int passwordtime = 2;
 const int addr = 7;	
-int main(int argc, char **argv, char **envp){
+const int passworderrorthreshold = 5;
 
-	const int gpioread, gpiowrite;
+void check(bool inputpassword[], bool realpassword[], int readpin);
+void getpassword(int readpin, bool realpassword[]);
+int geterror(bool inputpassword[] ,bool realpassword[]);
+void readPassword(bool* password);
+
+int main(int argc, char **argv, char **envp){
+	//INITAL VARIABLES-----------------------
 	bool inputpassword[ticrate*passwordtime];
-	relayDriverInit(addr);
+	bool realpassword[ticrate*passwordtime];
+	bool locked = true;
+	int gpioread, gpiowrite;
+	
 	gpiowrite = atoi(argv[1]);
 	gpioread = atoi(argv[2]);
+
+	//read realpassword from file
+	readPassword(realpassword);
+	//SETUP PINS-----------------------------	
+	relayDriverInit(addr);
 
 	gpio_request(gpiowrite, NULL);
 	gpio_request(gpioread, NULL);
@@ -23,26 +42,38 @@ int main(int argc, char **argv, char **envp){
 	gpio_direction_output(gpiowrite,0);
 	bool val = 1;
 
+	//MAIN LOOP-----------------------------
 	while(true){
-		if(gpio_get_value(gpioread)){
-			relaySetChannel(addr,0,1);
+		if(locked){
+			if(gpio_get_value(gpioread)){
+				check(inputpassword, realpassword, gpioread);
+			}
 		}else{
-			relaySetChannel(addr,0,0);
-		}
+			//check for hold down
+			if(gpio_get_value(gpioread)){
 
+			}
+		}
 		usleep(1000000/ticrate);
 	}
-
+	//FREE PINS AND CLOSE FILES------------
 	gpio_free(gpioread);
 	gpio_free(gpiowrite);
 
 	return 0;
 }
 
+//check if password is right
+void check(bool inputpassword[], bool realpassword[], int readpin){
+	getpassword(readpin, inputpassword);
+	int val = geterror(inputpassword,realpassword);
 
-void getpassword(int readpin, int passwordinput[]){
+}
+
+//retreive password from pressure sensor
+void getpassword(int readpin, bool passwordinput[]){
 	int tics = ticrate*passwordtime;
-	int index = 0
+	int index = 0;
 	while(index < tics){
 		passwordinput[index] = gpio_get_value(readpin);
 		usleep(1000000/ticrate);
@@ -50,11 +81,34 @@ void getpassword(int readpin, int passwordinput[]){
 	}
 }
 
-int geterror(int inputpassword[] ,int realpassword[]){
+//calculate error between inputted password and real password
+int geterror(bool inputpassword[] ,bool realpassword[]){
 	int tics = ticrate*passwordtime;
 	int error = 0;
 	while(tics--){
 		error += !(inputpassword[tics] == realpassword[tics]);
 	}	
 	return error;
+}
+
+//get the real password from the file
+void readPassword(bool password[]){
+	bool done = false;
+	char filename[] = {"password.txt"};
+	int maxLineLength = ticrate*passwordtime;
+	char line[maxLineLength];	
+	ifstream passFile;
+	passFile.open(filename);
+	if(!passFile.is_open()){
+		//log
+	}	
+	while(!done){ //Read from files
+		if(!passFile.getline(line, maxLineLength+1)){ //Get next line
+			done = true;
+		}
+		for(int i=0; i<maxLineLength; i++){
+			password[i] = (bool)(line[i]-48);
+		}
+	}	
+	passFile.close();
 }
