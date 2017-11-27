7315489b9fdfff4e00aaa4a896ddcb95b22f2ff2
Rob Farlow <robfarlow@gmail.com>
2017-11-27 17:53:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,32 +16,26 @@ const int ticrate = 20;
 const int passwordtime = 2;
 const int relayAddress = 7;	
 const int passworderrorthreshold = 5;
+bool inputpassword[200];
+bool realpassword[200];
 ofstream logfile;
 bool locked;
 //FUNCTION SIGNATURES------------------------
-void check(bool inputpassword[], bool realpassword[], int readpin);
-void checkForOnes(bool inputPassword[],int readpin);
-void getpassword(int readpin, bool realpassword[]);
-int geterror(bool inputpassword[] ,bool realpassword[]);
-void readPassword(bool* password);
+void check(int readpin);
+void checkForOnes(int readpin);
+void getpassword(int readpin);
+int geterror();
+void readPassword();
 void toggleLock();
 string getTime();
-string log(string x);
+void log(string x);
 int main(int argc, char **argv, char **envp){	
-	//INITAL VARIABLES-----------------------
-	bool inputpassword[ticrate*passwordtime];
-	bool realpassword[ticrate*passwordtime];
-
 	locked = true;
 	logfile.open("logfile.txt");
-	//log("INFO: Startn' the program.");
 	int gpiowrite = atoi(argv[1]);
 	int gpioread = atoi(argv[2]);
-	int motorpin = atoi(argv[3]);
 	//read realpassword from file
-	readPassword(realpassword);
-
-
+	readPassword();
 	//SETUP PINS-----------------------------	
 	relayDriverInit(relayAddress);
 
@@ -54,12 +48,19 @@ int main(int argc, char **argv, char **envp){
 
 	//MAIN LOOP-----------------------------
 	while(true){
-
 		if(locked){
 			if(gpio_get_value(gpioread)){
-				log("INFO: Starting to record user input");
+				log("INFO: Starting to record user input.");				
+				check(gpioread);
+				string inputie = "INFO: Recorded Input: ";
+				for(int i = 0; i<ticrate*passwordtime; i++){
+					if(inputpassword[i]){
+						inputie += "1";
+					}else{inputie+="0";}
+				}
+				cout << inputie;
+				log(inputie);
 				return 0;
-				check(inputpassword, realpassword, gpioread);
 			}
 		}
 
@@ -80,22 +81,28 @@ int main(int argc, char **argv, char **envp){
 }
 
 //check if password is right
-void check(bool inputpassword[], bool realpassword[], int readpin){
-	getpassword(readpin, inputpassword);
-	int val = geterror(inputpassword,realpassword);
+void check(int readpin){
+	getpassword(readpin);
+	int val = geterror();
+	string valstr = to_string(val);	
+	log("INFO: erorr value: " + valstr);
+	
 	if(val <= passworderrorthreshold){
-		//unlock
+		log("INFO: Password matched.");
+		toggleLock();
 	}
 	else{
-		//buzz
+		log("INFO: Password does not match.");
 	}
+	
 
 }
-void checkForOnes(bool inputPassword[],int readpin){
+//checking if pressure sensor is held down for 2 seconds
+void checkForOnes(int readpin){
 	bool shouldLock = true;
-	getpassword(readpin,inputPassword);
+	getpassword(readpin);
 	for(int i=0; i<(ticrate*passwordtime) && shouldLock; i++){
-		if(!inputPassword[i]){
+		if(!inputpassword[i]){
 			shouldLock = false;
 		}
 	}
@@ -105,45 +112,45 @@ void checkForOnes(bool inputPassword[],int readpin){
 }
 
 //retreive password from pressure sensor
-void getpassword(int readpin, bool passwordinput[]){
+void getpassword(int readpin){
 	int tics = ticrate*passwordtime;
 	int index = 0;
 	while(index < tics){
-		passwordinput[index] = gpio_get_value(readpin);
+		inputpassword[index] = gpio_get_value(readpin);
 		usleep(1000000/ticrate);
 		index++;
 	}
 }
 
 //calculate error between inputted password and real password
-int geterror(bool inputpassword[] ,bool realpassword[]){
+int geterror(){
 	int tics = ticrate*passwordtime;
 	int error = 0;
-	while(tics--){
-		error += !(inputpassword[tics] == realpassword[tics]);
+	int index = 0;
+	
+	while(index < tics){
+		if (inputpassword[index] != realpassword[index]){
+			error++;
+		}
+		index++;
 	}	
+	
 	return error;
 }
 
 //get the real password from the file
-void readPassword(bool password[]){
+void readPassword(){
 	bool done = false;
-	char filename[] = {"password.txt"};
+	char filename[] = {"newPassword.txt"};
 	int maxLineLength = ticrate*passwordtime;
-	char line[maxLineLength];	
+	string line;
 	ifstream passFile;
 	passFile.open(filename);
-	if(!passFile.is_open()){
-		//log
-	}	
-	while(!done){ //Read from files
-		if(!passFile.getline(line, maxLineLength+1)){ //Get next line
-			done = true;
-		}
-		for(int i=0; i<maxLineLength; i++){
-			password[i] = (bool)(line[i]-48);
-		}
-	}	
+	getline(passFile,line);
+	for(int i=0; i<maxLineLength; i++){
+		realpassword[i] = (bool)(line.at(i)-48);
+	}
+		
 	passFile.close();
 	log(line);
 }
@@ -156,10 +163,11 @@ string getTime(){
 	convert = convert.substr(0, convert.size()-1); // removes the /n from the string 
 	return convert;
 }
-string log(string x){
+void log(string x){
 	logfile << getTime() +" "+ x +"\n";
 }
 void toggleLock(){
+	log("INFO: Opening/Closing door.");
 	relaySetChannel(relayAddress, 0, 1);
 	sleep(1);
 	relaySetChannel(relayAddress, 0, 0);
